4621c1f3be06b5f165dd0cf4ad11621843d99a30
Raweeroj Thongdee <fewlove.007@gmail.com>
2021-08-19 00:39:25 +0700

    add ASP.NET

--- New folder/New folder/myApp/Program.cs
+++ New folder/New folder/myApp/Program.cs
@@ -7,7 +7,7 @@ namespace myApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("The current time is "+ DateTime.Now);
+            Console.WriteLine("The current time is " + DateTime.Now);
         }
     }
 }
