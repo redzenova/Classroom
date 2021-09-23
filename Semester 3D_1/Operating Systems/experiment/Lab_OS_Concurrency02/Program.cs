ce601f234d9227ff7b5f887cbafba330ce873552
Raweeroj Thongdee <fewlove.007@gmail.com>
2021-09-23 10:44:33 +0700

    add OS experimental

---
+++ Semester 3D_1/Operating Systems/experiment/Lab_OS_Concurrency02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab_OS_Concurrency02
+{
+    class Program
+    {
+        static int resource = 10000;
+        static void TestThread1()
+        {
+            resource = 55555;
+        }
+
+        static void Main(string[] args)
+        {
+            Thread th1 = new Thread(TestThread1);
+            th1.Start();
+            Thread.Sleep(10);
+            Console.WriteLine("resource={0}", resource);    
+        }
+    }
+}
